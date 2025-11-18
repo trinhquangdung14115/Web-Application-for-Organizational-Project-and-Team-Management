163c877d4003f44eea0b05ae9aee2dfccd4be293
mduc <minhductn0408@gmail.com>
2025-11-19 01:19:47 +0700

    Lead/mduc review (#37)
    * update main and app in FE
    
    * add app.js
    
    * Install compass and configuration
    
    * add vercel
    
    * finish add vercel
    
    * chore: add @hello-pangea/dnd for kanban
    
    * add scripts
    
    * fix lai
    
    * Fix start script path for Render deploy
    
    * fix register direct into home
    
    * add gg
    
    * add GG login
    
    * add GG login and fix api route
    
    * finish gg login
    
    * add interceptor check token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,39 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api";
-
 const axiosInstance = axios.create({
-  baseURL: `${API}`, 
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+// (Request Interceptor)
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Tự động lấy token từ kho và gắn vào mỗi request
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response; 
+  },
+  (error) => {
+    // Nếu Backend trả về 401 (Hết hạn hoặc Fake Token)
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      // về trang login 
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 export default axiosInstance;
